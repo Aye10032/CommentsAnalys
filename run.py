49e8d5fabac1d7b3a1e74b5e0f65a9301682cb08
Aye10032 <aye10032@gmail.com>
2024-06-14 10:11:37 +0800

    封装函数

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ class MissionType(IntEnum):
     CHOOSE = 1
 
 
-def load_llm():
+def load_llm() -> ChatOpenAI:
     with open('api_key.yaml', 'r') as yaml_f:
         yaml_text = yaml_f.read()
     llm_api = yaml.load(yaml_text, Loader=yaml.FullLoader)['zhipu']
@@ -51,7 +51,7 @@ def load_llm():
         model="glm-4",
         openai_api_base='https://open.bigmodel.cn/api/paas/v4/',
         openai_api_key=llm_api,
-        streaming=True
+        temperature=0,
     )
 
     return llm
@@ -92,7 +92,7 @@ def answer(lists: list, comment: str, mission_type: MissionType) -> dict:
     return result
 
 
-def load_data(data_path: str, init: bool = False) -> Tuple[DataFrame, list]:
+def load_data(data_path: str, output_path: str, init: bool = False) -> Tuple[DataFrame, list]:
     if init:
         with open(data_path, 'r', encoding='utf-8') as f:
             comments = f.readlines()
@@ -100,25 +100,22 @@ def load_data(data_path: str, init: bool = False) -> Tuple[DataFrame, list]:
         comment_df = pd.DataFrame([{'comment': comment.replace('\n', '')} for comment in comments])
         comment_df['generate'] = False
         comment_df['choose'] = False
-        comment_df.to_csv('data/news.csv', encoding='utf-8', index=False)
 
         opi_list = []
     else:
-        comment_df = pd.read_csv('data/news.csv', encoding='utf-8')
-        with open('data/ops.txt', 'r', encoding='utf-8') as op_f:
+        comment_df = pd.read_csv(os.path.join(output_path, 'news.csv'), encoding='utf-8')
+
+        with open(os.path.join(output_path, 'ops.txt'), 'r', encoding='utf-8') as op_f:
             opi_list = op_f.readlines()
             opi_list = [opi.replace('\n', '') for opi in opi_list]
 
     return comment_df, opi_list
 
 
-def main() -> None:
-    init = False
-
-    data, lists = load_data('data/news.txt', init)
-
-    # 截取前十条数据
-    data = data[:30]
+def run(data: DataFrame, lists: list, output_path: str, init: bool = False) -> None:
+    opinion_file = os.path.join(output_path, 'ops.txt')
+    record_file = os.path.join(output_path, 'news.csv')
+    result_file = os.path.join(output_path, 'result.csv')
 
     for index, row in tqdm(data.iterrows(), total=30):
         if row['generate']:
@@ -127,18 +124,18 @@ def main() -> None:
         comment = row['comment']
         lists.extend(answer(lists, comment, MissionType.UPDATE)['lists'])
         lists = list(set(lists))
-        with open('data/ops.txt', 'w', encoding='utf-8') as op_f:
+        with open(opinion_file, 'w', encoding='utf-8') as op_f:
             op_f.write('\n'.join(lists))
         data.at[index, 'generate'] = True
-        data.to_csv('data/news.csv', encoding='utf-8', index=False)
+        data.to_csv(record_file, encoding='utf-8', index=False)
 
-    if init or not os.path.exists('data/result.csv'):
+    if init or not os.path.exists(result_file):
         result = {key: 0 for key in lists}
         pd_list = [{'opi': key, 'count': value} for key, value in result.items()]
         df = pd.DataFrame(pd_list)
-        df.to_csv('data/result.csv', encoding='utf-8', index=False)
+        df.to_csv(result_file, encoding='utf-8', index=False)
     else:
-        df = pd.read_csv('data/result.csv', encoding='utf-8')
+        df = pd.read_csv(result_file, encoding='utf-8')
 
     for index, row in tqdm(data.iterrows(), total=30):
         if row['choose']:
@@ -147,12 +144,24 @@ def main() -> None:
         comment = row['comment']
         ans = answer(lists, comment, MissionType.CHOOSE)
         df.at[ans['index'], 'count'] += 1
-        df.to_csv('data/result.csv', encoding='utf-8', index=False)
+        df.to_csv(result_file, encoding='utf-8', index=False)
 
         data.at[index, 'choose'] = True
-        data.to_csv('data/news.csv', encoding='utf-8', index=False)
+        data.to_csv(record_file, encoding='utf-8', index=False)
 
-    df.to_csv('data/result.csv', encoding='utf-8', index=False)
+
+def main() -> None:
+    init = False
+    data_file = 'data/news.txt'
+    output_path = 'output'
+
+    data, lists = load_data(data_file, output_path, init)
+    # 截取前十条数据
+    data = data[:30]
+
+    os.makedirs(output_path, exist_ok=True)
+
+    run(data, lists, output_path, init)
 
 
 if __name__ == '__main__':
